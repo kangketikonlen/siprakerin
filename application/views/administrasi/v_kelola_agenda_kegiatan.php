f97813429164d9fc47189bcbc2979f31d7c77aa2
Gilang Pratama <pratamapriadi96@gmail.com>
2022-10-09 20:22:46 +0700

    Update design table mobile friendly

--- application/views/administrasi/v_kelola_agenda_kegiatan.php
+++ application/views/administrasi/v_kelola_agenda_kegiatan.php
@@ -1,8 +1,8 @@
 <div class="row">
 	<div class="col-lg-12">
 		<div class="card">
-			<div class="card-body">
-				<table id="dtTable" class="table table-sm table-bordered">
+			<div class="card-body table-responsive">
+				<table id="dtTable" class="table table-sm table-bordered table-hover text-nowrap">
 					<thead>
 						<th>No.</th>
 						<th>Tanggal</th>
